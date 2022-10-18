6931b24ff374e15ae924e77d58d3c9bc07c2612f
Lyndon <lyndon.stluce@gmail.com>
2022-10-17 20:48:32 -0400

    changed about me

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -67,29 +67,12 @@ export const Banner = () => {
                   <br></br>
                   <h2>About Me</h2>
                   <p>
-                    Out of all my jobs/projects that I`ve done, the ones I
-                    enjoyed the most are the types that allow me to create or
-                    rebuild something whether it's art, communication equipment
-                    or code and the greatest joy I get is when I see someone
-                    engaged in the final product of my work. What drives me to
-                    transition to Software Engineering is the fact that I get to
-                    be part of an evolving industry where I get to exercise my
-                    creativity and my ability to break down and solve problems.
-                    My greatest strengths are my creativity and perseverance, I
-                    was told this by those who I served in the military with.
-                    When presented with a difficult problem I would break it
-                    down to small problems or I would seek out those who had the
-                    same issue to get their input, or I would just step away and
-                    take a break to clear my head and then an idea or solution
-                    would come to me. As a team player I do my best to motivate
-                    those around me and to create a positive environment
-                    especially during the difficult times. One of the ways I do
-                    this is by showing my coworkers or fellow service members
-                    the comic I`ve been working on for years or one of my art
-                    works. When taking on a team project or a task I tend to
-                    take a backseat role if I feel the other members have a
-                    solid plan to execute the mission however if their methods
-                    are not working, I will jump in and take charge.
+                    Combat Veteran with 12 years of military service with
+                    experience in planning, maintenance and telecommunications
+                    seeking to transition into Software Engineering. Apart from
+                    my creativity, willingness to learn, and being result-driven
+                    my strengths are also my resilience to solve problems as
+                    well as to assist those who need help.
                   </p>
                 </div>
               )}
